66d9898b7698b20ea080d425ef8882b2cd9cbf15
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-04 23:19:46 +0530

    pass callback in tasktile

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -20,6 +20,11 @@ class _TasksListState extends State<TasksList> {
         return TaskTile(
           isChecked: tasks[index].isDone,
           taskTitle: tasks[index].name,
+          checkboxCallback: (bool? checkboxState) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
         );
       },
       itemCount: tasks.length,
