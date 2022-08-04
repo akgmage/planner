79605b3d6b9371f58cc48257ea3e8420100d9d68
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-04 10:46:53 +0530

    use modal to show tasks

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:tskmgt/models/task.dart';
 import 'package:tskmgt/widgets/tasks_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Something'),
+    Task(name: 'More'),
+    Task(name: 'More..'),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
+        TaskTile(taskTitle: tasks[0].name, isChecked: tasks[0].isDone),
+        TaskTile(taskTitle: tasks[1].name, isChecked: tasks[1].isDone),
+        TaskTile(taskTitle: tasks[2].name, isChecked: tasks[2].isDone),
       ],
     );
   }
