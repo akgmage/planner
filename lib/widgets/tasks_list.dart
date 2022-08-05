3b0a47e3628dc6fa651fbcd613be2f8f589a0215
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-05 23:10:52 +0530

    get data from provider

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:tskmgt/models/task.dart';
 import 'package:tskmgt/widgets/tasks_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:tskmgt/models/task_data.dart';
 
-class TasksList extends StatefulWidget {
-  final List<Task> tasks;
-  TasksList(this.tasks);
-  @override
-  State<TasksList> createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TaskTile(
-          isChecked: widget.tasks[index].isDone,
-          taskTitle: widget.tasks[index].name,
+          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
+          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
           checkboxCallback: (bool? checkboxState) {
-            setState(() {
-              widget.tasks[index].toggleDone();
-            });
+            // setState(() {
+            //   widget.tasks[index].toggleDone();
+            // });
           },
         );
       },
-      itemCount: widget.tasks.length,
+      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
