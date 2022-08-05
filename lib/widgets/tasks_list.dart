dbcee00c7e2c0357bbfbcf34e4d9a62c5acd8759
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-05 23:17:19 +0530

    wrap listview builder in a consumer widget

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,19 +6,23 @@ import 'package:tskmgt/models/task_data.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-          checkboxCallback: (bool? checkboxState) {
-            // setState(() {
-            //   widget.tasks[index].toggleDone();
-            // });
+    return Consumer<TaskData>(
+      builder: (BuildContext context, taskData, Widget? child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isChecked: taskData.tasks[index].isDone,
+              taskTitle: taskData.tasks[index].name,
+              checkboxCallback: (bool? checkboxState) {
+                // setState(() {
+                //   widget.tasks[index].toggleDone();
+                // });
+              },
+            );
           },
+          itemCount: taskData.tasks.length,
         );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
